4da8471cf232463b6763f67fcdd67bc16397441e
Nathan Knowler <nathan@knowler.dev>
2025-08-23 16:31:15 -0500

    Add pug to the mix

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
+import { renderFile } from "pug";
 
 const app = new Hono();
 
-app.get("*", c => c.text("Hello, World!"));
+app.use("*", (c, next) => {
+	c.setRenderer(
+		(template, ...data) => c.html(renderFile(`routes/${template}.pug`, ...data)),
+	);
+	next();
+});
+
+app.get("/", c => c.render("home"));
 
 export default app;
